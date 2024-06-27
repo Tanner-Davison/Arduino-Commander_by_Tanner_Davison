ca8e45c25d9179bcfeda502fc10a96fa421180a2
Tanner Davison <tanner.davison95@gmail.com>
2024-06-27 17:12:22 -0600

    updated code

--- arduinoPy.py
+++ arduinoPy.py
@@ -3,6 +3,7 @@ import serial.tools.list_ports
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 portList = []
+cmdList = ["ON", "OFF", "ALEXIS","CUSTOM"]
 
 for one in ports:
     portList.append(str(one))
@@ -20,8 +21,18 @@ serialInst.port = use
 serialInst.open()
 
 while True:
-    command = input("Arduino Command (ON/OFF/exit)")
-    serialInst.write(command.encode('utf-8'))
+    command = input("Arduino Command ex:(ON/OFF/ALEXIS/Custom/exit)")
 
-    if command == 'exit':
+    if command == 'CUSTOM':
+        if command is not None and command != "cmdList":
+            serialInst.write(command.encode('utf-8'))
+    elif command == 'exit':
+        serialInst.close()
         exit()
+    elif command == 'cmdList': 
+        for i in range(len(cmdList)):
+            print(cmdList[i])
+    elif command != 'exit':  
+        serialInst.write(command.encode('utf-8'))
+    else:
+        print("Invalid command. Please try again.")
